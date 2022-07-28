f913c94ce7098a2467f4500a1557601a3b62200b
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2022-07-28 23:31:52 +0100

    startup improvements

--- build/ojob_p1.sh
+++ build/ojob_p1.sh
@@ -6,6 +6,7 @@ nohup /usr/bin/ojob /home/openvscode-server/.p2.yaml $* > /tmp/.p2.log 2>&1 &
 # Start VSCode
 if [ ! -z $NOVSCODE ]; then
     echo Not starting VSCode
+    exec tail -f /dev/null
 else
     cd /home/workspace
     exec ${OPENVSCODE_SERVER_ROOT}/bin/openvscode-server --host 0.0.0.0 --without-connection-token -- $*
