20b28287cdc2f283b4ab2063925c45e6e7f5a94f
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2022-12-14 04:56:05 +0000

    update docker extension

--- build/extensions/update.sh
+++ build/extensions/update.sh
@@ -1,5 +1,5 @@
 wget https://open-vsx.org/api/redhat/vscode-yaml/1.10.1/file/redhat.vscode-yaml-1.10.1.vsix
 wget https://open-vsx.org/api/redhat/vscode-xml/0.22.0/file/redhat.vscode-xml-0.22.0.vsix
 wget https://open-vsx.org/api/GitHub/github-vscode-theme/6.3.2/file/GitHub.github-vscode-theme-6.3.2.vsix
-wget https://open-vsx.org/api/ms-azuretools/vscode-docker/1.23.1/file/ms-azuretools.vscode-docker-1.23.1.vsix
+wget https://open-vsx.org/api/ms-azuretools/vscode-docker/1.23.2/file/ms-azuretools.vscode-docker-1.23.2.vsix
 wget https://open-vsx.org/api/ms-kubernetes-tools/vscode-kubernetes-tools/1.3.10/file/ms-kubernetes-tools.vscode-kubernetes-tools-1.3.10.vsix
