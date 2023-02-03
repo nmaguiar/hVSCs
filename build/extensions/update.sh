151eb8909fdc6325d5421aec731fd426d656f189
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2023-02-03 03:15:03 +0000

    update to redhat xml 0.24.0

--- build/extensions/update.sh
+++ build/extensions/update.sh
@@ -1,6 +1,6 @@
 rm *.vsix
 wget https://open-vsx.org/api/redhat/vscode-yaml/1.11.0/file/redhat.vscode-yaml-1.11.0.vsix
-wget https://open-vsx.org/api/redhat/vscode-xml/0.23.0/file/redhat.vscode-xml-0.23.0.vsix
+wget https://open-vsx.org/api/redhat/vscode-xml/0.24.0/file/redhat.vscode-xml-0.24.0.vsix
 wget https://open-vsx.org/api/GitHub/github-vscode-theme/6.3.3/file/GitHub.github-vscode-theme-6.3.3.vsix
 wget https://open-vsx.org/api/ms-azuretools/vscode-docker/1.23.3/file/ms-azuretools.vscode-docker-1.23.3.vsix
 wget https://open-vsx.org/api/ms-kubernetes-tools/vscode-kubernetes-tools/1.3.11/file/ms-kubernetes-tools.vscode-kubernetes-tools-1.3.11.vsix
