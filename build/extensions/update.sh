dbd5207606e5894789b2c4379cddfbe379889c18
nmaguiar <nmaguiar@users.noreply.github.com>
2023-03-26 18:54:14 +0000

    upgrade radhat yaml extension

--- build/extensions/update.sh
+++ build/extensions/update.sh
@@ -1,5 +1,5 @@
 rm *.vsix
-wget https://open-vsx.org/api/redhat/vscode-yaml/1.12.1/file/redhat.vscode-yaml-1.12.1.vsix
+wget https://open-vsx.org/api/redhat/vscode-yaml/1.12.2/file/redhat.vscode-yaml-1.12.2.vsix
 wget https://open-vsx.org/api/redhat/vscode-xml/0.24.0/file/redhat.vscode-xml-0.24.0.vsix
 wget https://open-vsx.org/api/GitHub/github-vscode-theme/6.3.3/file/GitHub.github-vscode-theme-6.3.3.vsix
 wget https://open-vsx.org/api/ms-azuretools/vscode-docker/1.24.0/file/ms-azuretools.vscode-docker-1.24.0.vsix
