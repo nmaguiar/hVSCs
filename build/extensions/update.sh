5778e65525211cf2bddcf59a094a236bda9d4283
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2023-04-23 02:30:11 +0100

    update extensions

--- build/extensions/update.sh
+++ build/extensions/update.sh
@@ -1,6 +1,6 @@
 rm *.vsix
 wget https://open-vsx.org/api/redhat/vscode-yaml/1.12.2/file/redhat.vscode-yaml-1.12.2.vsix
-wget https://open-vsx.org/api/redhat/vscode-xml/0.24.0/file/redhat.vscode-xml-0.24.0.vsix
+wget https://open-vsx.org/api/redhat/vscode-xml/0.25.0/file/redhat.vscode-xml-0.25.0.vsix
 wget https://open-vsx.org/api/GitHub/github-vscode-theme/6.3.4/file/GitHub.github-vscode-theme-6.3.4.vsix
-wget https://open-vsx.org/api/ms-azuretools/vscode-docker/1.24.0/file/ms-azuretools.vscode-docker-1.24.0.vsix
+wget https://open-vsx.org/api/ms-azuretools/vscode-docker/1.25.0/file/ms-azuretools.vscode-docker-1.25.0.vsix
 wget https://open-vsx.org/api/ms-kubernetes-tools/vscode-kubernetes-tools/1.3.11/file/ms-kubernetes-tools.vscode-kubernetes-tools-1.3.11.vsix
