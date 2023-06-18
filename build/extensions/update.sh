e660e345f86d2a2148ed04e4bf7fd759245269bc
nmaguiar <nmaguiar@users.noreply.github.com>
2023-06-18 00:18:06 +0000

    bump kubernetes extension

--- build/extensions/update.sh
+++ build/extensions/update.sh
@@ -3,4 +3,4 @@ wget https://open-vsx.org/api/redhat/vscode-yaml/1.13.0/file/redhat.vscode-yaml-
 wget https://open-vsx.org/api/redhat/vscode-xml/0.25.0/file/redhat.vscode-xml-0.25.0.vsix
 wget https://open-vsx.org/api/GitHub/github-vscode-theme/6.3.4/file/GitHub.github-vscode-theme-6.3.4.vsix
 wget https://open-vsx.org/api/ms-azuretools/vscode-docker/1.25.1/file/ms-azuretools.vscode-docker-1.25.1.vsix
-wget https://open-vsx.org/api/ms-kubernetes-tools/vscode-kubernetes-tools/1.3.11/file/ms-kubernetes-tools.vscode-kubernetes-tools-1.3.11.vsix
+wget https://open-vsx.org/api/ms-kubernetes-tools/vscode-kubernetes-tools/1.3.13/file/ms-kubernetes-tools.vscode-kubernetes-tools-1.3.13.vsix
